a85dd4c3bd207e86b57dcc9c140f26939c076cfc
Andrew Gibbons <andrew.h.gibbons@protonmail.com>
2018-10-02 10:07:02 +0900

    Updated MeepLib.py

--- Optics/MeepLib.py
+++ Optics/MeepLib.py
@@ -1,6 +1,6 @@
 import meep as mp
 
-def geo1D_thin_film_si(film_n, si_n, ps_thickness, si_thickness, pos=0):
+def geo1D_thin_film_si(film_n, si_n, ps_thickness, si_thickness, pos=0, bare_substrate=False):
     ps_block = mp.Block(size=mp.Vector3(1,1,ps_thickness),
                                center = mp.Vector3(0,0,pos - (si_thickness + ps_thickness/2 )),
                                material = mp.Medium(epsilon = film_n**2))
@@ -9,5 +9,13 @@ def geo1D_thin_film_si(film_n, si_n, ps_thickness, si_thickness, pos=0):
                         center = mp.Vector3(0,0,pos - si_thickness/2),
                         material = mp.Medium(epsilon = si_n**2))
 
+    if bare_substrate:
+        geometry = [si_block]
+    else:
+        geometry = [ps_block, si_block]
     
-    return [ps_block, si_block]
+    return geometry
+
+class SimParams:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
---
+++ setup.py
@@ -0,0 +1,7 @@
+
+from distutils.core import setup
+
+setup(name="Sivaniah",
+	version="1.0",
+	author="Andrew H Gibbons",
+	author_email="andrew.h.gibbons@gmail.com")
